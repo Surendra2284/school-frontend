051f0ec96fec34cca8a866c5f23f183251cad847
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-09-24 14:31:03 +0530

    Fix: use process.env.PORT for Render

--- src/app/components/card-preview/card-preview.component.ts
+++ src/app/components/card-preview/card-preview.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { StudentExlDataService } from '../../services/StudentExlDataService.service';
 @Component({
   selector: 'app-card-preview',
   templateUrl: './card-preview.component.html',
-  styleUrls: ['./card-preview.component.css']
+  styleUrls: ['./card-preview.component.scss']
 })
-export class CardPreviewComponent {
+export class CardPreviewComponent implements OnInit {
+  students: any[] = [];
 
+  constructor(private studentService: StudentExlDataService) {}
+
+  ngOnInit(): void {
+    this.students = this.studentService.getStudents();
+  }
 }
