0f56604ea67b56a2e715bdedda2e599d47cbe552
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-09-27 00:28:53 +0530

    Update Angular frontend project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://r18hk424-3000.inc1.devtunnels.ms' // 🔁 Replace with your actual backend URL
+  apiUrl: 'https://school-backend-yqgp.onrender.com' // 🔁 Replace with your actual backend URL
 };
